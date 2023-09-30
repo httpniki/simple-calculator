f7aca444eec811085e95403784bf78ca31745c1e
Marcelo Nicolas Romani <marcelocbsn@gmail.com>
2023-09-30 16:31:16 -0300

    Create functionality to display keys value

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
-export default class Main() {
-   constructor(){
+import { type DisplayKeysValue } from './types/types.js'
+
+export default class Main {
+   private readonly disabledToDisplayKeys = ['=', 'C']
+   private readonly $keys = document.querySelectorAll('.key') as NodeListOf<HTMLInputElement>
+   private readonly $output = document.getElementById('output') as HTMLInputElement
+
+   constructor () {
       this.setup()
    }
 
-   private setup() {}
-} 
+   private setup (): void {
+      this.$keys.forEach((key) => {
+         key.addEventListener('click', (event) => {
+            const target = event.target as HTMLInputElement
+            const hasDisabledToDisplayKey = this.disabledToDisplayKeys.some((el) => el === target.value)
+
+            if (!target || !this.$output) return
+            if (target.value !== key.value || hasDisabledToDisplayKey) return
+
+            this.displayKeysValue({ target })
+         })
+      })
+   }
+
+   private displayKeysValue ({ target }: DisplayKeysValue): void {
+      this.$output.value = this.$output.value + target.value
+   }
+}
 
 new Main()
