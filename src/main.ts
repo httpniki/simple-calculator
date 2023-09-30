d42f4610ec89f6a9234624992f535ec7c4e82714
Marcelo Nicolas Romani <marcelocbsn@gmail.com>
2023-09-30 19:20:04 -0300

    Refactored display keys function and added functionality to solve and reset the output

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import { type DisplayKeysValue } from './types/types.js'
-
 export default class Main {
-   private readonly disabledToDisplayKeys = ['=', 'C']
+   private readonly disabledKeysToDisplay = ['C', '=']
    private readonly $keys = document.querySelectorAll('.key') as NodeListOf<HTMLInputElement>
    private readonly $output = document.getElementById('output') as HTMLInputElement
-
+   private output = ''
    constructor () {
       this.setup()
    }
@@ -13,18 +11,17 @@ export default class Main {
       this.$keys.forEach((key) => {
          key.addEventListener('click', (event) => {
             const target = event.target as HTMLInputElement
-            const hasDisabledToDisplayKey = this.disabledToDisplayKeys.some((el) => el === target.value)
+            const isDisabledToDisplay = !this.disabledKeysToDisplay.includes(key.value)
 
-            if (!target || !this.$output) return
-            if (target.value !== key.value || hasDisabledToDisplayKey) return
+            if (target.value === key.value && isDisabledToDisplay)
+               this.output = this.$output.value + target.value
 
-            this.displayKeysValue({ target })
+            if (target.value === 'C') this.output = ''
+            if (target.value === '=' && this.output) this.output = eval(this.$output.value).toString()
+
+            this.$output.value = this.output
          })
       })
-   }
-
-   private displayKeysValue ({ target }: DisplayKeysValue): void {
-      this.$output.value = this.$output.value + target.value
    }
 }
 
