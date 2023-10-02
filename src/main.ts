e0d3ca0f6cff3f16dd3371f43cfee05331315ec8
Marcelo Nicolas Romani <marcelocbsn@gmail.com>
2023-10-02 17:29:07 -0300

    Refactored app functions into unique functions

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,41 @@ export default class Main {
    private readonly disabledKeysToDisplay = ['C', '=']
    private readonly $keys = document.querySelectorAll('.key') as NodeListOf<HTMLInputElement>
    private readonly $output = document.getElementById('output') as HTMLInputElement
-   private valueToDisplay = ''
+   private valueToRender = ''
    constructor () {
       this.setup()
    }
 
-   private setup (): void {
+   private setup(): void {
       this.$keys.forEach((key) => {
          key.addEventListener('click', (event) => {
             const target = event.target as HTMLInputElement
-            const isDisabledToDisplay = !this.disabledKeysToDisplay.includes(key.value)
 
             if (target.value !== key.value) return
-            if (this.$output.value === '0') this.valueToDisplay = target.value
 
-            if (isDisabledToDisplay && this.$output.value !== '0')
-               this.valueToDisplay = this.$output.value + target.value
+            if (target.value === 'C') this.clearOutput()
+            if (target.value === '=' && this.valueToRender) this.solve()
 
-            if (target.value === 'C') this.valueToDisplay = '0'
-            if (target.value === '=' && this.valueToDisplay) this.valueToDisplay = eval(this.$output.value).toString()
-
-            this.$output.value = this.valueToDisplay
+            this.showOutputValue(target.value)
          })
       })
+   }
+
+   private clearOutput(): void {
+      this.valueToRender = '0'
+   }
+
+   private solve(): void {
+      this.valueToRender = eval(this.$output.value).toString()
+   }
+
+   private showOutputValue(value: string = ''): void {
+      const isDisabledToDisplay = !this.disabledKeysToDisplay.includes(value)
+
+      if (isDisabledToDisplay && this.$output.value === '0') this.valueToRender = value
+      if (isDisabledToDisplay && this.$output.value !== '0') this.valueToRender = this.$output.value + value
+
+      this.$output.value = this.valueToRender
    }
 }
 
