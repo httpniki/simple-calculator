1ae1013d4ce30a07c2c6080f17dc9d44a8e6b2ba
Marcelo Nicolas Romani <marcelocbsn@gmail.com>
2023-10-02 19:46:44 -0300

    Create keybinds

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,50 @@ export default class Main {
    private valueToRender = ''
    constructor () {
       this.setup()
+      this.keybinds()
    }
 
    private setup(): void {
       this.$keys.forEach((key) => {
          key.addEventListener('click', (event) => {
             const target = event.target as HTMLInputElement
-
             if (target.value !== key.value) return
 
             if (target.value === 'C') this.clearOutput()
             if (target.value === '=' && this.valueToRender) this.solve()
 
-            this.showOutputValue(target.value)
+            this.updateOutputValue(target.value)
+            this.showOutputValue()
          })
       })
+   }
+
+   private keybinds(): void {
+      const keys = [...Array(10).keys(), '*', '/', '-', '+'].map(key => key.toString())
+
+      document.addEventListener('keydown', (event) => {
+         switch (event.key) {
+         case keys.filter(key => key === event.key)[0]:
+            this.updateOutputValue(event.key)
+            this.showOutputValue()
+            break
+         case 'Enter':
+            this.solve()
+            this.showOutputValue()
+            break
+         }
+      })
+   }
+
+   private updateOutputValue(value: string): void {
+      if (this.disabledKeysToDisplay.includes(value)) return
+
+      if (this.$output.value === '0') this.valueToRender = value
+      if (this.$output.value !== '0') this.valueToRender = this.$output.value + value
+   }
+
+   private showOutputValue(): void {
+      this.$output.value = this.valueToRender
    }
 
    private clearOutput(): void {
@@ -28,15 +57,6 @@ export default class Main {
 
    private solve(): void {
       this.valueToRender = eval(this.$output.value).toString()
-   }
-
-   private showOutputValue(value: string = ''): void {
-      const isDisabledToDisplay = !this.disabledKeysToDisplay.includes(value)
-
-      if (isDisabledToDisplay && this.$output.value === '0') this.valueToRender = value
-      if (isDisabledToDisplay && this.$output.value !== '0') this.valueToRender = this.$output.value + value
-
-      this.$output.value = this.valueToRender
    }
 }
 
